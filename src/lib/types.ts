9bf2abb43eb0d921bb7ea9bc33f3e018f7f6bcc7
nicomelendez <nicomelendez10@gmail.com>
2024-08-19 18:26:10 -0300

    tag en la interfaz de info

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,52 +1,53 @@
 export interface Contact {
-  id: number;
-  icon: any;
-  url: string;
+  id: number
+  icon: any
+  url: string
 }
 
 export interface Proyect {
-  id: number;
-  title: string;
-  description: string;
-  resumen: string;
-  image: string;
-  url: string;
-  repository: string;
-  online: boolean;
-  technologies: any;
+  id: number
+  title: string
+  description: string
+  resumen: string
+  image: string
+  url: string
+  repository: string
+  online: boolean
+  technologies: any
 }
 
 export interface Technology {
-  id: number;
-  img: string;
-  class: string;
-  span: string;
+  id: number
+  img: string
+  class: string
+  span: string
 }
 
 export interface TechnologiesProyect {
-  idProyecto: number;
-  techonolohy: Technology[];
+  idProyecto: number
+  techonolohy: Technology[]
 }
 
 export interface Study {
-  id: number;
-  title: string;
-  descripcion: string;
-  initialDate: string;
-  finalyDate: string;
-  finally: boolean;
+  id: number
+  title: string
+  descripcion: string
+  initialDate: string
+  finalyDate: string
+  finally: boolean
 }
 
 export interface Info {
-  id: number;
-  titulo: string;
-  resumen: string;
-  fecha: string;
-  img: string;
-  descripcion: string;
-  path:string;
-  drive:string;
-  link?: string;
-  github?:string;
-  aprendisaje:string;
+  id: number
+  titulo: string
+  resumen: string
+  fecha: string
+  img: string
+  descripcion: string
+  path: string
+  drive: string
+  link?: string
+  github?: string
+  aprendisaje: string
+  tags: any
 }
