757f023dfe0fcd076c06234ec1f8f100b3297a46
nicomelendez <nicomelendez10@gmail.com>
2024-08-23 17:59:31 -0300

    Arreglo de version de vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,34 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import { VitePWA } from "vite-plugin-pwa"
+import { VitePWA } from "vite-plugin-pwa";
 import vercel from "@astrojs/vercel/serverless";
+
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   devToolbar: {
-		enabled: false,
-	},
+    enabled: false
+  },
   integrations: [tailwind()],
   output: "server",
   adapter: vercel(),
   vite: {
-		build: {
-			cssMinify: "lightningcss",
-		},
-		ssr: {
-			noExternal: ["path-to-regexp"],
-		},
-		plugins: [
-			VitePWA({
-				registerType: "autoUpdate",
-				workbox: {
-					globDirectory: "dist",
-					globPatterns: ["**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}"],
-					// Don't fallback on document based (e.g. `/some-page`) requests
-					// This removes an errant console.log message from showing up.
-					navigateFallback: null,
-				},
-			}),
-		],
-	},
+    build: {
+      cssMinify: "lightningcss"
+    },
+    ssr: {
+      noExternal: ["path-to-regexp"]
+    },
+    plugins: [VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globDirectory: "dist",
+        globPatterns: ["**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}"],
+        // Don't fallback on document based (e.g. `/some-page`) requests
+        // This removes an errant console.log message from showing up.
+        navigateFallback: null
+      }
+    })]
+  }
 });
